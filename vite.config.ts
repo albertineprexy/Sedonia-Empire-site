5c8926d1017a78b61d36e607d1c2ba5903bc1d1d
albertineprexy <aleghemo23@gmail.com>
2026-01-17 00:29:41 +0100

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import viteStaticCopy from 'vite-plugin-static-copy';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: './',   //
+      base: './',
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [react()],
+      plugins: [
+        react(),
+        viteStaticCopy({
+          targets: [
+            {
+              src: 'public/_redirects',
+              dest: ''
+            }
+          ]
+        })
+      ],
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
